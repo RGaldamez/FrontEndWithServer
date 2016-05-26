3a132d0542e2839b4ef47d79f4bf8b536c29d027
Ricardo Galdámez <ricardo.j.galdamez@gmail.com>
2016-05-25 19:55:58 -0600

    testing

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var inert = require('inert');
 
 var server = new hapi.Server();
 server.connection({
-    port: ~~process.env.PORT || 8001
+    port: 'mongodb://admin:admin@ds051848.mlab.com:51848/rgdbb'
 });
 
 server.register(inert, function(err){
